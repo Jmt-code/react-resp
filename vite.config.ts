465295013b1b4dc5e53318f63f5d3ba27a6c6800
Juanma torres <juanma.trrs@gmail.com>
2024-04-28 22:35:09 +0200

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   "base": "https://Jmt-code.github.io/react-resp",
+  build: {
+    outDir: 'dist', // Directorio de salida para los archivos de producción
+    sourcemap: true, // Generar archivos de mapas de fuente para depuración
+    minify: 'esbuild' // Minificación utilizando esbuild
+  },
 })
